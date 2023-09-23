383cc3a146a95787e638076002498cc7173a0638
Eduardo <eduardotejedadelgado@gmail.com>
2023-09-23 07:52:32 -0400

    Create ShoppingListForm.jsx

--- src/ShoppingListForm.jsx
+++ src/ShoppingListForm.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
 function ShoppingListForm() {
-    return <h1>Shopping</h1>
+    const [formData, setFormData] = useState({product:"", quantity: 0})
+    const handleChange = (evt) => {
+        setFormData(currData => {
+            return {
+                ...currData,
+                [evt.target.name]: evt.target.value
+            }
+        })
+    }
+
+
+    return (
+        <form>
+            <h1>Product is: {formData.product} </h1>
+            <label htmlFor="product">Product Name</label>
+            <input type="text"
+            placeholder="product name"
+            name="product"
+            id="product"
+            onChange={handleChange}
+            value={FormData.product} 
+            />
+            
+        </form>
+    )
 }
 
 export default ShoppingListForm;
